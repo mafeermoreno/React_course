3ae0fd01b3e86748461f99499ae685b814cf765b
mafeermoreno <marifermg29@gmail.com>
2024-01-10 22:44:20 -0600

    Use of arrays

---
+++ FundamentosJS/js/08.js
@@ -0,0 +1,11 @@
+// Arrays o Arreglos
+//Nota: Los arreglos o arrays son de tipo object en JavaScript, no hay como tal un tipo array en JS
+const tecnologias = [20, 30, true, "React", "JavaScript"]
+
+// Imprimir una propiedad, se le pone el número del índice
+console.log(tecnologias[3])
+console.table(tecnologias)
+// Regresa el número de propiedades dentro de un array 
+console.log(tecnologias.length)
+// Imprime todo el arreglo como un string
+console.log(tecnologias.toString())
