3d80b6407da66e2f6568a00e49ec45c90aa947cd
mafeermoreno <marifermg29@gmail.com>
2024-01-12 17:30:18 -0600

    Create functions

---
+++ FundamentosJS/js/12.js
@@ -0,0 +1,10 @@
+// Funciones - Function declaration
+
+function sumar(numero = 0, numero2 = 0) {
+    console.log(numero2)
+    console.log(numero + numero2)
+}
+sumar(10, 20)
+sumar(2, 3)
+sumar(100)
+sumar()
