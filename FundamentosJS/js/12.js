0cedcfa5b8b1909469ee20a5ef79d5f3bbe4de3f
mafeermoreno <marifermg29@gmail.com>
2024-01-12 17:42:57 -0600

    Use functions

--- FundamentosJS/js/12.js
+++ FundamentosJS/js/12.js
@@ -1,10 +1,29 @@
 // Funciones - Function declaration
 
-function sumar(numero = 0, numero2 = 0) {
+/* function sumar(numero = 0, numero2 = 0) {
     console.log(numero2)
     console.log(numero + numero2)
 }
 sumar(10, 20)
 sumar(2, 3)
 sumar(100)
-sumar()
+sumar() */
+
+/* function sumar(numero = 0, numero2 = 0) {
+    return [numero + numero2, "hola Mundo"]
+} 
+//Destructuring arrays
+const [resultado, texto] = sumar(20, 30)
+console.log(resultado)
+console.log(texto)*/
+
+function sumar(numero = 0, numero2 = 0) {
+    return {
+        resultado: numero + numero2, texto: "hola Mundo"
+    }
+}
+
+//Destructuring object
+const {resultado, texto} = sumar(20, 30)
+console.log(resultado)
+console.log(texto)
